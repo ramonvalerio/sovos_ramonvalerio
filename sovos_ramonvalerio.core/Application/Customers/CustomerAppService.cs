7c9401e59534cea48f83e6cf7b97b46f055d17d9
RamonValerio <ramonvalerios@gmail.com>
2020-06-05 20:33:35 -0300

    Created more a Application Layer to separate responsabilities

---
+++ sovos_ramonvalerio.core/Application/Customers/CustomerAppService.cs
@@ -0,0 +1,35 @@
+﻿using sovos_ramonvalerio.core.Domain.Customers;
+using sovos_ramonvalerio.core.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace sovos_ramonvalerio.core.Application.Customers
+{
+    public class CustomerAppService : ICustomerAppService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerAppService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public IEnumerable<Customer> Return_a_list_of_customers_without_orders()
+        {
+            return _customerRepository.GetAll();
+        }
+
+        public void Add_a_new_Customer(CustomerCommand command)
+        {
+            var customer = new Customer(command.Name, command.Email);
+
+            if (customer.IsValid())
+            {
+                _customerRepository.Add(customer);
+                return;
+            }
+
+            throw new Exception("Invalid Customer.");
+        }
+    }
+}
---
+++ sovos_ramonvalerio.core/Application/Customers/ICustomerAppService.cs
@@ -0,0 +1,11 @@
+﻿using sovos_ramonvalerio.core.Domain.Customers;
+using System.Collections.Generic;
+
+namespace sovos_ramonvalerio.core.Application.Customers
+{
+    public interface ICustomerAppService
+    {
+        IEnumerable<Customer> Return_a_list_of_customers_without_orders();
+        void Add_a_new_Customer(CustomerCommand command);
+    }
+}
