bd392168b14d0547216a72779a3461c3d432e282
RamonValerio <ramonvalerios@gmail.com>
2020-06-05 17:39:41 -0300

    - Created test project - Preparing test's classes

---
+++ sovos_ramonvalerio.core.test/Domain/CustomerTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace sovos_ramonvalerio.core.test.Domain
+{
+    public class CustomerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ sovos_ramonvalerio.core.test/Domain/OrderTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace sovos_ramonvalerio.core.test.Domain
+{
+    public class OrderTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
